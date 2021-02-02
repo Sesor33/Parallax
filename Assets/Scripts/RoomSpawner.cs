7a2b665d8a406bb6e38a3550ec234a5e188cfcdd
Tech <sesor333@aol.com>
2021-02-01 20:00:45 -0500

    Rooms generate, but with dead ends.

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -6,17 +6,47 @@ public class RoomSpawner : MonoBehaviour
 {
     public int opening; //1, bot. 2, top. 3, left. 4, right. This is what door the point NEEDS
 
-    private void Update() {
-        switch (opening) {
-            case 1:
-            //Need bot
-            case 2:
-            //Need top
-            case 3:
-            //Need left
-            case 4:
-            //Need right
-            break;
+    private RoomTemplates templates;
+
+    private int rngRngPleaseBeNiceToMe;
+    private bool isSpawned = false;
+
+    void Start() {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("SpawnNewRoom", 0.25f);
+    }
+
+    void SpawnNewRoom() {
+        if (isSpawned == false) {
+            switch (opening) {
+                case 1:
+                    rngRngPleaseBeNiceToMe = Random.Range(0, templates.botRooms.Length);
+                    Instantiate(templates.botRooms[rngRngPleaseBeNiceToMe], transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    rngRngPleaseBeNiceToMe = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rngRngPleaseBeNiceToMe], transform.position, Quaternion.identity);
+                    break;
+                //Need top
+                case 3:
+                    rngRngPleaseBeNiceToMe = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rngRngPleaseBeNiceToMe], transform.position, Quaternion.identity);
+                    break;
+                //Need left
+                case 4:
+                    rngRngPleaseBeNiceToMe = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rngRngPleaseBeNiceToMe], transform.position, Quaternion.identity);
+                    break;
+                    //Need right
+            }
+            isSpawned = true;
+        }
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("SpawnPoint") ) {
+            Destroy(gameObject);
         }
     }
 }
