3cca846843f95bb4dd268704cf1baec2dd0d20d5
Tech <sesor333@aol.com>
2021-02-01 19:31:04 -0500

    Working on directional spawning

---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int opening; //1, bot. 2, top. 3, left. 4, right. This is what door the point NEEDS
+
+    private void Update() {
+        switch (opening) {
+            case 1:
+            //Need bot
+            case 2:
+            //Need top
+            case 3:
+            //Need left
+            case 4:
+            //Need right
+            break;
+        }
+    }
+}
---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] botRooms;
+    public GameObject[] topRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] leftRooms;
+}
