f42c651d53d1c386e92995f69d1a437abf83f848
Tech <sesor333@aol.com>
2021-02-14 15:42:42 -0500

    Camera can now follow player

---
+++ Assets/Scripts/SmoothCam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SmoothCam : MonoBehaviour {
+    public Transform objectToFollow;
+    public Vector3 camOffset;
+    [Range(1, 10)]
+    public float smoothing;
+
+    private void Start() {
+        Invoke("setPlayerFollow", 0.1f);
+    }
+
+    private void FixedUpdate() {
+        FollowObject();
+    }
+
+    private void setPlayerFollow() {
+        objectToFollow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FollowObject() {
+        Vector3 targetPos = objectToFollow.position + camOffset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, smoothing * Time.fixedDeltaTime);
+        transform.position = smoothPos;
+    }
+}
