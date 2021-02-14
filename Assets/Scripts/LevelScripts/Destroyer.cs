0bcb4a8a71f8b8826a9395042443f96f152304be
Tech <sesor333@aol.com>
2021-02-14 16:13:53 -0500

    Moved scripts around to be easier to access

--- Assets/Scripts/LevelScripts/Destroyer.cs
+++ Assets/Scripts/LevelScripts/Destroyer.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-   void OnTriggerEnter2D(Collider2D other) {
+
+    private RoomTemplates templates;
+
+    private void Start() {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Decoration") {
             Destroy(other.gameObject);
         }
 
         if (other.gameObject.tag == "Room") {
+            templates.rooms.Remove(other.gameObject);
             Destroy(other.gameObject);
         }
         
