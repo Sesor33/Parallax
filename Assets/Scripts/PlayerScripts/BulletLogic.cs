0005bd713425b2b8b08377c74d6293f2e3e0e176
Tech <sesor333@aol.com>
2021-02-26 13:09:39 -0500

    Bullets work

--- Assets/Scripts/PlayerScripts/BulletLogic.cs
+++ Assets/Scripts/PlayerScripts/BulletLogic.cs
@@ -6,6 +6,8 @@ public class BulletLogic : MonoBehaviour
 {
     public float speed;
     public float bulletLifetime;
+    public float dist;
+    public LayerMask solidObjects;
 
     public GameObject destroyEffect;
 
@@ -16,6 +18,15 @@ public class BulletLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, dist, solidObjects);
+        if (hitInfo.collider != null) {
+            if (hitInfo.collider.CompareTag("Enemy")) {
+                //Enemy goes boom
+                Debug.Log("Enemy takes damage here");
+            }
+            DestroyBullet();
+        }
+
         transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
 
