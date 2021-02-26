c46095aac280239803b12a43c53c880ccecc2f5b
Tech <sesor333@aol.com>
2021-02-26 12:44:22 -0500

    Bullets fire and destroy after (x) seconds

---
+++ Assets/Scripts/OtherScripts/DestroyBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBullet : MonoBehaviour
+    
+{
+    public float effectTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyThis", effectTime);
+    }
+
+    void DestroyThis() {
+        Destroy(gameObject);
+    }
+}
