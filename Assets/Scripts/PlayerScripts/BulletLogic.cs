0a54fc3e90b9549384a243b26059568128554323
Tech <sesor333@aol.com>
2021-02-26 12:22:03 -0500

    Start of projectile system

---
+++ Assets/Scripts/PlayerScripts/BulletLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    public float speed;
+    public float bulletLifetime;
+
+    public GameObject destroyEffect;
+
+    private void Start() {
+        Invoke("DestroyBullet", bulletLifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void DestroyBullet() {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/WeaponLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLogic : MonoBehaviour
+{
+    public float offset;
+
+    public GameObject bullet;
+    public Transform startingPoint;
+
+    private float timeBetweenShots;
+    public float startTimeBetweenShots;
+
+    private void Update() {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + offset);
+
+        if (timeBetweenShots <= 0) {
+            if (Input.GetMouseButtonDown(0)) {
+                Instantiate(bullet, startingPoint.position, transform.rotation);
+                timeBetweenShots = startTimeBetweenShots;
+            }
+
+            
+        }
+        else {
+            timeBetweenShots -= Time.deltaTime;
+        }
+    }
+
+        
+
+}
