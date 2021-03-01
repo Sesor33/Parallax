b6aee300354e6098bad953952eac8b31411b510b
Tech <sesor333@aol.com>
2021-03-01 13:09:53 -0500

    Starting to work on audio

---
+++ Assets/Scripts/OtherScripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+
+{
+    public Sound[] clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/OtherScripts/Sound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public AudioClip clip;
+
+    public float vol;
+    public float pitch;
+}
