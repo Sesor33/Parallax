ea86da56804af9fc07e8a2a2655a049490f9d5d9
Tech <sesor333@aol.com>
2021-03-08 13:20:34 -0500

    Exit can now transition levels

--- Assets/Scripts/LevelScripts/InteractableExit.cs
+++ Assets/Scripts/LevelScripts/InteractableExit.cs
@@ -7,11 +7,13 @@ public class InteractableExit : MonoBehaviour
     public bool isInRange;
     public KeyCode interactKey;
 
+    public GameManager gm;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -19,8 +21,22 @@ public class InteractableExit : MonoBehaviour
     {
         if (isInRange) {
             if (Input.GetKeyDown(interactKey)) {
-
+                gm.LoadNextLevel();
             }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            isInRange = true;
+            Debug.Log("In range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            isInRange = false;
+            Debug.Log("Out of range");
         }
     }
 }
