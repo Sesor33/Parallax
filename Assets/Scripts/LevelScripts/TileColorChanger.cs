17eeb9ebaada371228602b1239186916f1e834d0
Tech <sesor333@aol.com>
2021-03-09 12:55:08 -0500

    Can now differentiate between two levels

---
+++ Assets/Scripts/LevelScripts/TileColorChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileColorChanger : MonoBehaviour
+{
+    private Renderer spriteRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend = GetComponent<Renderer>();
+
+        switch (GameManager.floor) {
+            case 1:
+                break;
+            case 2:
+                spriteRend.material.color = new Color32(0,0,200,255);
+                break;
+        }
+    }
+
+    
+}
--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!GameManager.playerIsDead) {
             float h = Input.GetAxis("Horizontal");
