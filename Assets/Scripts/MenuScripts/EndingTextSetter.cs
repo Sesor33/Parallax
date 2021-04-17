8db1bf76a2a1c6722368b31919578614314ed052
Tech <sesor333@aol.com>
2021-04-17 16:31:41 -0400

    Ending Screen implemented

---
+++ Assets/Scripts/MenuScripts/EndingTextSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndingTextSetter : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI destroyedRobotsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destroyedRobotsText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        destroyedRobotsText.SetText(GameManager.enemiesKilled.ToString());
+
+    }
+
+}
