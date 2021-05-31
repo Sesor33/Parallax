12b9318369aaf5e88678007423cc313853eb1809
Tech <sesor333@aol.com>
2021-05-31 00:47:57 -0400

    Working on sniper laser

---
+++ Assets/SniperLaser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperLaser : MonoBehaviour
+{
+    private LineRenderer laser;
+    private Transform target;
+
+    private RaycastHit2D hitInfo;
+
+    public int laserMaxRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindObjectOfType<Player>().transform;
+        laser = gameObject.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        laser.SetPosition(0, transform.position);
+        if (Vector3.Distance(target.position, transform.position) <= laserMaxRange) {
+            laser.SetPosition(1, target.transform.position);
+        }
+
+        else {
+            laser.SetPosition(1, transform.position);
+        }
+    }
+}
